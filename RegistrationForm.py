371c9dfe338ac1c51cac2a8d892970900db3906b
Saranya2003 <62011240@KMITL.AC.TH>
2021-05-15 20:13:40 +0700

    [ADDED] GUI for registration form

--- RegistrationForm.py
+++ RegistrationForm.py
@@ -1,3 +1,17 @@
 import sys
 from PySide6.QtWidgets import *
 from PySide6.QtCore import *
+from registrationScreen import Ui_registerForm
+
+class registrationFormUI(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self, None)
+
+        self.ui = Ui_registerForm()
+        self.ui.setupUi(self)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    w = registrationFormUI()
+    w.show()
+    sys.exit(app.exec_())
